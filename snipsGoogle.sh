4baf82228fe44b0aba360b285b9b0fda48dedd3c
Krysztof Błachowicz <krzysztof.blachowicz@gmail.com>
2019-08-21 23:41:38 +0200

    httpie support

--- snipsGoogle.sh
+++ snipsGoogle.sh
@@ -1,6 +1,3 @@
-#!/usr/bin/env bash
-cd "${0%/*}"
-# Shell script to use VoiceRSS as Snips TTS
 # Install mpg123: sudo apt-get install mpg123
 # Set your cache path
 cache="/usr/share/snips/tts_cache/"
@@ -10,13 +7,14 @@ apiKey="bbb1ba806a254ca5882a8f84b296b9c4"
 # Edit /etc/snips.toml
 # Set "customtts" as snips-tts provider
 #
-# Add as customtts f.ex.: command = ["/home/pi/snipsWavenet.sh", "%%OUTPUT_FILE%%", "%%LANG%%", "%%TEXT%%"]
+# Add as customtts f.ex.: command = ["/home/pi/snipsWavenet.sh", "%%OUTPUT_FILE%%", "%%LANG%%", "%%TEXT%%", "curl|httpie"]
 # Change "PL" to another language country code, "GB" per exemple for a british voice
 # Restart snips: systemctl restart snips-*
 
 outfile="$1"
 lang="$2"
 text="$3"
+method="$4"
 
 mkdir -pv "$cache"
 
@@ -26,24 +24,38 @@ text=${text//\'/\\\'}
 md5string="$text""$lang"
 hash="$(echo -n "$md5string" | md5sum | sed 's/ .*$//')"
 
-cachefile="$cache""$hash".wav
-downloadFile="/tmp/""$hash"
+cachefile="$cache$hash".wav
+downloadFile="$cache$hash".mp3
 
 if [[ ! -f "$cachefile" ]]; then
-    curl -G -v \
-    -A "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.125 Safari/537.36" \
-    --data-urlencode "ie=UTF-8" \
-    --data-urlencode "client=tw-ob" \
-    --data-urlencode "q=$text" \
-    --data-urlencode "tl=$lang" \
-    --data-urlencode "total=1" \
-    --data-urlencode "idx=0" \
-    --data-urlencode "textlen=1" \
-    "https://translate.google.com/translate_tts" > "$downloadFile.mp3"
+    if [ "$method" = "httpie" ]; then
+        http --download --output "$downloadFile" \
+        "https://translate.google.com/translate_tts" \
+        ie==UTF-8 \
+        client==tw-ob \
+        q=="$text" \
+        tl=="$lang" \
+        total==1 \
+        idx==0 \
+        textlen==1 \
+        User-Agent:"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.125 Safari/537.36"
+    else
+        curl -G -v \
+        -A "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.125 Safari/537.36" \
+        --data-urlencode "ie=UTF-8" \
+        --data-urlencode "client=tw-ob" \
+        --data-urlencode "q=$text" \
+        --data-urlencode "tl=$lang" \
+        --data-urlencode "total=1" \
+        --data-urlencode "idx=0" \
+        --data-urlencode "textlen=1" \
+        "https://translate.google.com/translate_tts" > "$downloadFile"
+    fi
 
-    mpg123 --quiet --wav "$cachefile" "$downloadFile".mp3
-    rm "$downloadFile.mp3"
+    mpg123 --quiet --wav "$cachefile" "$downloadFile"
+    rm "$downloadFile"
 fi
 
 touch "$cachefile"
+#echo "$cachefile" "$outfile"
 cp "$cachefile" "$outfile"
